0209fac64263d68c3def1e4b1f06d3e4d212677e
nikita <koz.nikita@gmail.com>
2023-07-19 00:12:03 +0300

    point cloud test

---
+++ Assets/_project/Scripts/Test/CloudPointTest.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class CloudPointTest : MonoBehaviour
+{
+    [SerializeField] private ARPointCloudManager _pointsCloud;
+
+
+    private void StartFill()
+    {
+    }
+}
