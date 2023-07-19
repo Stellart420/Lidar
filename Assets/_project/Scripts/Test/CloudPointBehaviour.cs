eb48ab4d53cbee2b04790d7f09c14db9dce5eaaa
nikita <koz.nikita@gmail.com>
2023-07-19 20:27:15 +0300

    cloud point next

--- Assets/_project/Scripts/Test/CloudPointBehaviour.cs
+++ Assets/_project/Scripts/Test/CloudPointBehaviour.cs
@@ -1,15 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 
 public class CloudPointBehaviour : MonoBehaviour
 {
-    private static int _pointsCount = 0;
+    private Transform _transform;
+    private Transform _pointPrefab;
 
-    void Start()
+    private Dictionary<Vector3, GameObject> _pointsView = new Dictionary<Vector3, GameObject>();
+
+
+    public void Initialize(Transform pointPrefab)
     {
-        Debug.Log($"Add point: {_pointsCount}");
-        ++_pointsCount;
+        _transform = transform;
+        _pointPrefab = pointPrefab;
     }
 
+    public void UpdatePoints(NativeSlice<Vector3>? positions)
+    {
+        if (positions == null || !positions.HasValue)
+            return;
+
+        int created = 0;
+        foreach(var position in positions)
+        {
+            if (_pointsView.ContainsKey(position))
+                continue;
+
+            var newPoint = Instantiate(_pointPrefab, _transform);
+            newPoint.localPosition = position;
+            newPoint.localScale = Vector3.one * 0.01f;
+
+            _pointsView.Add(position, newPoint.gameObject);
+
+            ++created;
+        }
+
+        Debug.Log($"PointsCount: {created}");
+
+    }
 }
--- Assets/_project/Scripts/Test/CloudPointTest.cs
+++ Assets/_project/Scripts/Test/CloudPointTest.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class CloudPointTest : MonoBehaviour
 {
     [SerializeField] private ARPointCloudManager _pointsCloud;
+    [SerializeField] private Transform _pointPrefab;
 
+    private Dictionary<TrackableId, CloudPointBehaviour> _clouds = new Dictionary<TrackableId, CloudPointBehaviour>();
 
     private void Start()
     {
@@ -15,6 +18,31 @@ public class CloudPointTest : MonoBehaviour
 
     private void _pointsCloud_pointCloudsChanged(ARPointCloudChangedEventArgs obj)
     {
-        Debug.Log($"CHANGES. ADD: {obj.added.Count} UPDATE: {obj.updated.Count} REMOVE: {obj.removed.Count}");
+        foreach(var added in obj.added)
+        {
+            var behaviour = added.GetComponent<CloudPointBehaviour>();
+            if (behaviour != null)
+            {
+                _clouds.Add(added.trackableId, behaviour);
+                behaviour.Initialize(_pointPrefab);
+                behaviour.UpdatePoints(added.positions);
+            }
+            else
+                Debug.LogError("Can not find behaviour component");
+        }
+
+        foreach (var updated in obj.updated)
+        {
+
+            if(_clouds.TryGetValue(updated.trackableId, out CloudPointBehaviour behaviour))
+            {
+                behaviour.UpdatePoints(updated.positions);
+            }
+            else
+                Debug.LogError("Can not find behaviour component");
+        }
+
+        if (obj.removed.Count > 0)
+            Debug.Log($"REMOVED: {obj.removed.Count}");
     }
 }
